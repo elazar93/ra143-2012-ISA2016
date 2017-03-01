3169d1856239a4b34513b4a556c2dad015ced9e8
Straja12 <prefiks12@gmail.com>
2017-03-01 21:44:05 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app =express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var appRoutes = require('./app/routes/api')(router);
+var path	= require('path');
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
+app.use('/api',appRoutes);
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/data',function(err){
+
+	if(err){
+
+		console.log("not working"+ err);
+
+	} else {
+		console.log("working mongodb");
+	}
+});
+
+
+app.get('*', function(req,res){
+
+	res.sendFile(path.join(__dirname+'/public/app/views/index.html'));
+
+});
+
+app.listen(8081, function(){
+
+	console.log('Run run run');
+});
